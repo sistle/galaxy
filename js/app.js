4db62e6d6bdd1c90e07f45eb79e2c7c16bd77b59
sistle <jekich93@gmail.com>
2022-07-14 17:02:23 +0300

    Updates

--- js/app.js
+++ js/app.js
@@ -92,3 +92,15 @@ new Swiper(".intro-slider", {
     },
   },
 });
+
+const header = document.querySelector(".header");
+
+function headeHendler(entries, observer) {
+  if (entries[0].isIntersecting) {
+    header.classList.remove("scroll");
+  } else {
+    header.classList.add("scroll");
+  }
+}
+const headerObserver = new IntersectionObserver(headeHendler, { threshold: 1 });
+headerObserver.observe(header);
